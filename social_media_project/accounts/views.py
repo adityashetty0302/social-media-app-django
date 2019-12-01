ef190b774beaad4bb3d3b82cbd3ca3db6967b864
aditya <adityashetty0302@gmail.com>
2019-12-02 01:21:51 +0530

    added signup view & user model

--- social_media_project/accounts/views.py
+++ social_media_project/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import UserCreateForm
 
-# Create your views here.
+
+class SignUp(CreateView):
+    form_class = UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
--- social_media_project/social_media_project/urls.py
+++ social_media_project/social_media_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.HomePage.as_view(), name="home"),
     path('admin/', admin.site.urls),
 ]
