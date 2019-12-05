63f96656aae299c3e0ba4d186290cb56179b621a
Aditya Shetty <aditya.shetty@niiconsulting.com>
2019-12-05 19:13:57 +0530

    project completed

--- social_media_project/accounts/urls.py
+++ social_media_project/accounts/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
          auth_views.LoginView.as_view(template_name="accounts/login.html"),
          name='login'),
     path('logout/',
-         auth_views.LogoutView.as_view(template_name="accounts/logout.html"),
-         name='logout'),
+         auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
 ]
