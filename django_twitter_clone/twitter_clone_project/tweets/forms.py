7619cb30ca79d4b3f2d72ca39282df4a99d19484
aditya <adityashetty0302@gmail.com>
2019-12-06 23:22:57 +0530

    project completed

--- django_twitter_clone/twitter_clone_project/tweets/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-
-class TweetsForm(forms.ModelForm):
-    class Meta:
-        fields = ("tweet",)
-        model = models.Tweets
--- django_twitter_clone/twitter_clone_project/tweets/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'tweets'
-
-urlpatterns = [
-    path('new/', CreateTweet.as_view(), name='new')
-]
